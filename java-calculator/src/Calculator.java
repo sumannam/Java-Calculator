278722e48ef403a5d203f1709e689796183ac090
smnam-laptop <sumannam@gmail.com>
2025-09-25 11:09:01 +0900

    계산기 v1.0 소스 파일

---
+++ java-calculator/src/Calculator.java
@@ -0,0 +1,40 @@
+public class Calculator {
+    int num1;
+    int num2;
+    int result;
+
+    public void setNumbers(int num1, int num2)
+    {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int add()
+    {
+        result = num1 + num2;
+        return result;
+    }
+
+    public int subtract()
+    {
+        return 0;
+    }
+
+    public int multiply()
+    {
+        result = 0;
+        for(int i = 0; i < num2; i++)
+            result = result + num1;
+        return result;
+    }
+
+    public int divide()
+    {
+        return 0;
+    }
+
+    public void displayResult(String operator)
+    {
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+    }
+}
---
+++ java-calculator/test/CalculatorTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        Calculator calc = new Calculator();
+        calc.setNumbers(10, 20);
+        assertEquals(30, calc.add());
+    }
+
+    @org.junit.jupiter.api.Test
+    void subtract() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void multiply() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void divide() {
+    }
+}

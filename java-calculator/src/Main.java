667623a9681fef137e339735eb803a1728397797
smnam-laptop <sumannam@gmail.com>
2025-10-01 10:46:26 +0900

    main에서 플러스 기능만 남김

--- java-calculator/src/Main.java
+++ java-calculator/src/Main.java
@@ -6,15 +6,5 @@ public class Main {
         calc_add.setNumbers(5, 3);
         calc_add.add();
         calc_add.displayResult("+");
-
-        Calculator calc_sub = new Calculator();
-
-        Calculator calc_mul = new Calculator();
-        calc_mul.setNumbers(5, 3);
-        calc_mul.multiply();
-        calc_mul.displayResult("*");
-
-        Calculator calc_div = new Calculator();
-        calc_div.setNumbers(10, 2);
     }
 }

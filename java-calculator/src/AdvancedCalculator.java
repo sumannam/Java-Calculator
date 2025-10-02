efdb4f7dc07d843a14e88bb8e743c6030422a6ac
smnam-laptop <sumannam@gmail.com>
2025-10-02 11:32:39 +0900

    AdvancedCalculator 주석 추가

--- java-calculator/src/AdvancedCalculator.java
+++ java-calculator/src/AdvancedCalculator.java
@@ -1,5 +1,9 @@
 public class AdvancedCalculator extends Calculator {
+    // 계산기 v2.0 개발
+    // 이슈 : https://github.com/sumannam/Java-Calculator/issues/5
 
+    // 곱셈 연산자로 두 수 곱하기 완료
+    // https://github.com/sumannam/Java-Calculator/issues/6
     @Override
     public int multiply()
     {
@@ -7,6 +11,8 @@ public class AdvancedCalculator extends Calculator {
         return result;
     }
 
+    // 나누셈 연산자로 두 수 나누기 완료
+    // https://github.com/sumannam/Java-Calculator/issues/7
     @Override
     public int divide()
     {

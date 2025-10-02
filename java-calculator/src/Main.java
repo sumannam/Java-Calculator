8bab6beaa2027f55a64ab6c4143c1b98e4fde911
smnam-laptop <sumannam@gmail.com>
2025-10-02 13:23:10 +0900

    AdvancedCalculator 개발 완료
    https://github.com/sumannam/Java-Calculator/issues/1

--- java-calculator/src/Main.java
+++ java-calculator/src/Main.java
@@ -6,5 +6,10 @@ public class Main {
         calc_add.setNumbers(5, 3);
         calc_add.add();
         calc_add.displayResult("+");
+
+        AdvancedCalculator calc_mul = new AdvancedCalculator();
+        calc_mul.setNumbers(10, 2);
+        calc_mul.multiply();
+        calc_mul.displayResult("*");
     }
 }
